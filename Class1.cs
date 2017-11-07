21ee3b60bd46d1370ab41e2b27117ab7f3c838b0
dvall032 <33464939+dvall032@users.noreply.github.com>
2017-11-07 14:46:01 -0500

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -10,7 +10,7 @@ namespace GameProject
 {
     public class Game
     {
-        //change ONE
+        //change PEEL
         public string Team1 { get; set; }
         public string Team2 { get; set; }
         public int Team1Score { get; set; }
